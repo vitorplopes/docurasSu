f276ab23f371cf9ad49d0825f1e702ec8352cfbc
Madruga <Madruga@Madruga-PC>
2013-02-04 08:19:04 -0200

    Atualizacao de plugins.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,7 +49,7 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
-        runtime ":resources:1.1.6"
+        runtime ":resources:1.2.RC2"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
@@ -58,7 +58,7 @@ grails.project.dependency.resolution = {
 
         build ":tomcat:$grailsVersion"
 
-        runtime ":database-migration:1.2.1"
+        runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
 		
